8837411db465ce29bb85a67a22d2692d54d0f9ae
Vivek Lingayat <vvkvivekv@gmail.com>
2018-02-18 11:37:26 +0530

    Add Comments and TODO SplashScreen

--- app/src/main/java/com/udacity/exploreindia/SplashActivity.java
+++ app/src/main/java/com/udacity/exploreindia/SplashActivity.java
@@ -9,20 +9,40 @@ import android.os.Bundle;
 import com.udacity.exploreindia.helper.PrefManager;
 
 public class SplashActivity extends AppCompatActivity {
+
+    // "PrefManager" used in app to store data locally (eg. firstLaunch)
     PrefManager prefManager;
     Context context = this;
 
+    // "secondsDelayed = (seconds)" used to delay while moving from splash screen
     int secondsDelayed = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        // -                                                                                         declarations
         prefManager = new PrefManager(context);
+
+        // TODO: (In future) can fetch updated list of States and CityTown supported.
+        // TODO: (In future) check app updated needed.
+
+        // -                                                                                         actions
+        
+        // Checking app Launched First Time
         if (prefManager.isFirstTimeLaunch()) {
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
+
+                    /*
+                     * if first time launch move activity to WelcomeActivity
+                     *
+                     * On WelcomeActivity:
+                     *   Introduction to application
+                     *   Getting permission from user (eg. Location permission).
+                     */
+
                     startActivity(new Intent(context, WelcomeActivity.class));
                     finish();
                 }
@@ -31,6 +51,9 @@ public class SplashActivity extends AppCompatActivity {
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
+
+                    // if not first time launch move activity to MainActivity
+
                     startActivity(new Intent(context, MainActivity.class));
                     finish();
                 }
