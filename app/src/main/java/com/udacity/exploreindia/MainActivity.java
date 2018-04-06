b379c02b435ec726b7faab080f0403c4f91f9a23
Rachna Shriwas <rachnashriwas@gmail.com>
2018-04-06 16:23:03 +0530

    check for location in MainActivity

--- app/src/main/java/com/udacity/exploreindia/MainActivity.java
+++ app/src/main/java/com/udacity/exploreindia/MainActivity.java
@@ -2,6 +2,7 @@ package com.udacity.exploreindia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +11,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        //Check for Location Permission
+        GPSTracker gps = new GPSTracker(MainActivity.this);
+        if(gps.canGetLocation()){
+
+            double latitude = gps.getLatitude();
+            double longitude = gps.getLongitude();
+
+            Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+        }else{
+            // can't get location
+            // GPS or Network is not enabled
+            // Ask user to enable GPS/network in settings
+            gps.showSettingsAlert();
+        }
 
     }
 }
