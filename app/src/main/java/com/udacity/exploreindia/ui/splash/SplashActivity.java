926d368f7e345caba81b6a83b848ec3e2b174c40
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-04-13 13:19:10 +0530

    Refactor code, Limit scopes

--- app/src/main/java/com/udacity/exploreindia/ui/splash/SplashActivity.java
+++ app/src/main/java/com/udacity/exploreindia/ui/splash/SplashActivity.java
@@ -1,6 +1,5 @@
 package com.udacity.exploreindia.ui.splash;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -12,32 +11,27 @@ import com.udacity.exploreindia.ui.main.MainActivity;
 import com.udacity.exploreindia.ui.welcome.WelcomeActivity;
 
 public class SplashActivity extends AppCompatActivity {
-    PreferenceRepository prefRepo;
-    Context context = this;
 
-    int secondsDelayed = 1;
+    private static final int SECONDS_DELAYED = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        prefRepo = new PreferenceRepository(context);
-        if (prefRepo.isFirstTimeLaunch()) {
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    startActivity(new Intent(context, WelcomeActivity.class));
-                    finish();
-                }
-            }, secondsDelayed * 1000);
-        } else {
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    startActivity(new Intent(context, MainActivity.class));
-                    finish();
-                }
-            }, secondsDelayed * 1000);
-        }
+
+        PreferenceRepository prefRepo = new PreferenceRepository(getApplicationContext());
+
+        if (prefRepo.isFirstTimeLaunch()) launchActivity(WelcomeActivity.class);
+        else launchActivity(MainActivity.class);
+    }
+
+    private void launchActivity(final Class activityClass) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, activityClass));
+                finish();
+            }
+        }, SECONDS_DELAYED * 1000);
     }
 }
