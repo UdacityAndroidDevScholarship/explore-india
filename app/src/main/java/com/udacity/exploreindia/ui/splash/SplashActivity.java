e340bd43771c54e3d18e75c7ec89e4028db52cbe
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-04-13 11:32:09 +0530

    Add/Extend Base Activity and Fragment

--- app/src/main/java/com/udacity/exploreindia/ui/splash/SplashActivity.java
+++ app/src/main/java/com/udacity/exploreindia/ui/splash/SplashActivity.java
@@ -3,14 +3,14 @@ package com.udacity.exploreindia.ui.splash;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 
 import com.udacity.exploreindia.R;
 import com.udacity.exploreindia.repo.PreferenceRepository;
+import com.udacity.exploreindia.ui.base.BaseActivity;
 import com.udacity.exploreindia.ui.main.MainActivity;
 import com.udacity.exploreindia.ui.welcome.WelcomeActivity;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends BaseActivity {
 
     private static final int SECONDS_DELAYED = 1;
 
