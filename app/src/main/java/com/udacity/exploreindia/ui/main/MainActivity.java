54f2a168f1a878a696a8836c04c7c6317bba1155
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-04-13 11:30:05 +0530

    Package by feature

--- app/src/main/java/com/udacity/exploreindia/ui/main/MainActivity.java
+++ app/src/main/java/com/udacity/exploreindia/ui/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.udacity.exploreindia;
+package com.udacity.exploreindia.ui.main;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.udacity.exploreindia.R;
 
 public class MainActivity extends AppCompatActivity {
 
