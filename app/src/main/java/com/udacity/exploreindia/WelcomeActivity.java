9f44bc33eb3d4c0826266830a919f0b57fe34643
tavishjain <jaintavish@yahoo.com>
2018-04-17 22:11:31 +0530

    Added the location permission in welcome activity
    Added the location permission in welcome activity.

--- app/src/main/java/com/udacity/exploreindia/WelcomeActivity.java
+++ app/src/main/java/com/udacity/exploreindia/WelcomeActivity.java
@@ -1,9 +1,14 @@
 package com.udacity.exploreindia;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -22,6 +27,8 @@ public class WelcomeActivity extends AppCompatActivity {
         tvTest = findViewById(R.id.a_welcome_tv_test);
         prefManager = new PrefManager(context);
 
+        locationPermissionCheck();
+
         tvTest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,5 +38,25 @@ public class WelcomeActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public void locationPermissionCheck() {
+        String[] location_permission = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,location_permission,1);
+        } else {
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 1) {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.v("Permission", "Permission: " + permissions[0] + "was " + grantResults[0]);
+            } else { }
+        }
     }
 }
