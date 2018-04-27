c9cb156b5d49dfda7c5f41ef16222e0f9864ec55
Nishank Sanjeev Samant <nishanksamant@gmail.com>
2018-04-28 03:41:13 +0530

    Fix #43 - Design Login Screen UI

--- app/src/main/java/com/udacity/exploreindia/ui/login/LoginActivity.java
+++ app/src/main/java/com/udacity/exploreindia/ui/login/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.udacity.exploreindia.ui.login;
 
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -18,7 +19,8 @@ public class LoginActivity extends BaseActivity<LoginContract.Presenter,Activity
 
     @Override
     protected void init(@Nullable Bundle savedInstanceState) {
-
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 
     @Override
