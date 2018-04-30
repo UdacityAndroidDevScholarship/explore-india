c53b925c361ec71f2732227663a78f5039dfe017
Intkhab Ahmed <intkhab.ahmed64@gmail.com>
2018-04-30 22:40:56 +0530

    Implemented place detail layout, edited SplashActivity, PresenterFactory and HomeActivity for testing purpose and can be changed later, added helper strings,ids and dimensions and drawable

--- app/src/main/java/com/udacity/exploreindia/ui/splash/SplashActivity.java
+++ app/src/main/java/com/udacity/exploreindia/ui/splash/SplashActivity.java
@@ -1,11 +1,13 @@
 package com.udacity.exploreindia.ui.splash;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import com.udacity.exploreindia.base.BaseActivity;
 import com.udacity.exploreindia.R;
 import com.udacity.exploreindia.databinding.ActivitySplashBinding;
+import com.udacity.exploreindia.ui.home.HomeActivity;
 
 public class SplashActivity extends BaseActivity< SplashContract.Presenter,ActivitySplashBinding> implements SplashContract.View {
 
@@ -28,6 +30,8 @@ public class SplashActivity extends BaseActivity< SplashContract.Presenter,Activ
 
     @Override
     public void moveToNextSScreen() {
-
+        Intent intent = new Intent(this, HomeActivity.class);
+        finish();
+        startActivity(intent);
     }
 }
