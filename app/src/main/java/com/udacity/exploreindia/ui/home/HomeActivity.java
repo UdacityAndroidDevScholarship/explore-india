59df7d68248ce9cb287fb37c4486b34f081bb626
codesqills <codesqills@gmail.com>
2018-05-04 02:38:06 -0400

    Fix #59 -Liked Places Screen

--- app/src/main/java/com/udacity/exploreindia/ui/home/HomeActivity.java
+++ app/src/main/java/com/udacity/exploreindia/ui/home/HomeActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import com.udacity.exploreindia.R;
 import com.udacity.exploreindia.base.BaseActivity;
 import com.udacity.exploreindia.databinding.ActivityHomBinding;
+import com.udacity.exploreindia.ui.home.fragments.likedplaces.LikedPlacesFragment;
 
 public class HomeActivity extends BaseActivity<HomeContract.Presenter, ActivityHomBinding> implements HomeContract.View {
 
@@ -18,7 +19,11 @@ public class HomeActivity extends BaseActivity<HomeContract.Presenter, ActivityH
 
     @Override
     protected void init(@Nullable Bundle savedInstanceState) {
-
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, new LikedPlacesFragment())
+                    .commit();
+        }
     }
 
     @Override
